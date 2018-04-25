88d3d1b7182438412fe3960c0362fe8f3692f64e
teasacura <teasacuraster@gmail.com>
2018-04-25 18:36:25 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,47 @@
+require "pry"
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  @@genres_hash = Hash.new(0)
+  @@artists_hash = Hash.new(0)
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    @@genres.each do |genre|
+        @@genres_hash[genre] += 1
+    end
+    @@genres_hash
+  end
+
+  def self.artist_count
+    @@artists.each do |artist|
+        @@artists_hash[artist] += 1
+    end
+    @@artists_hash
+  end
+
+end
